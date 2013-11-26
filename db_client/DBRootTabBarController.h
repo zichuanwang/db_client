9ba1b29934c7f898db262ea2ce8a184df72ad6ee
bluebitch <wzc345@gmail.com>
2013-11-25 21:29:30 -0800

    UI layout.

---
+++ db_client/DBRootTabBarController.h
@@ -0,0 +1,13 @@
+//
+//  DBRootTabBarController.h
+//  db_client
+//
+//  Created by 王 紫川 on 13-11-25.
+//  Copyright (c) 2013年 USC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DBRootTabBarController : UITabBarController
+
+@end
---
+++ db_client/DBRootTabBarController.m
@@ -0,0 +1,40 @@
+//
+//  DBRootTabBarController.m
+//  db_client
+//
+//  Created by 王 紫川 on 13-11-25.
+//  Copyright (c) 2013年 USC. All rights reserved.
+//
+
+#import "DBRootTabBarController.h"
+
+@interface DBRootTabBarController ()
+
+@end
+
+@implementation DBRootTabBarController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UI methods
+
+@end
